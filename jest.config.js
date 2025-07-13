d182d939e6c1b1c9719305b716797940a137b349
Dario <dgimenez.developer@gmail.com>
2025-07-13 11:27:21 -0300

    fix: corregir configuración de Jest y agregar alias @/
    - Corregir referencia setupFilesAfterEnv de .ts a .js
    - Agregar soporte para alias @/ apuntando a src/
    - Configurar identity-obj-proxy para importaciones CSS

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,9 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'], // Cambio: .js en lugar de .ts
   moduleNameMapper: {
-    '\\.(css|less|scss|sass)$': 'identity-obj-proxy'
-  }
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+    '^@/(.*)$': '<rootDir>/src/$1', // Añadido: soporte para alias @/
+  },
 };
